b84f8448c4569d7c18a662e78c2cc7b40c163935
Hardikgiri Gosai <hardik.gosai007@gmail.com>
2023-12-22 14:57:26 +0530

    Add project files.

---
+++ Getri_TokenAuthentication/EntityFramework/GetriTokenAuthenticationContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Getri_TokenAuthentication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Getri_TokenAuthentication.EntityFramework;
+
+public partial class GetriTokenAuthenticationContext : DbContext
+{
+    public GetriTokenAuthenticationContext()
+    {
+    }
+
+    public GetriTokenAuthenticationContext(DbContextOptions<GetriTokenAuthenticationContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblUser> TblUser { get; set; }
+}
---
+++ Getri_TokenAuthentication/Models/TblUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Getri_TokenAuthentication.Models;
+
+public partial class TblUser
+{
+    public int UserId { get; set; }
+
+    public string? FullName { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Password { get; set; }
+}
